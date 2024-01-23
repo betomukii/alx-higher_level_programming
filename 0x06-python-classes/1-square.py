b0ffb864bca86e7323049066d917a8b601471fd5
betomukii <mukiibeatrice4@gmail.com>
2024-01-24 01:29:08 +0300

    initializing size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Defines a square """
+
+class Square:
+    """Representing a square """
+
+    def __init__(self, size):
+        """ Initializing square class
+        args: size represents the size of the square"""
+
+        self.__size = size
