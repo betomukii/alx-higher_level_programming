b80a2217ec87390f2145995be35d2219bf1bf05d
betomukii <mukiibeatrice4@gmail.com>
2024-01-30 15:11:15 +0300

    changes

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -17,55 +17,55 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def widht(self):
-            """retrieves width attribute"""
-            return self.__width
+    @property
+    def width(self):
+        """retrieves width attribute"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """sets width attribute"""
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """sets width attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """retrieves height attribute"""
-            return self.__height
+    @property
+    def height(self):
+        """retrieves height attribute"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """sets height attribute"""
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """sets height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
 
-        def area(self):
-            """returns area"""
-            return (self.__width * self.__height)
+    def area(self):
+        """returns area"""
+        return (self.__width * self.__height)
 
-        def perimeter(self):
-           """returns the perimeter"""
-           if self.__width == 0 or self.__height == o:
-               return (0)
-           return ((self.__width * 2) + (self.__height * 2))
+    def perimeter(self):
+       """returns the perimeter"""
+       if self.__width == 0 or self.__height == o:
+           return (0)
+       return ((self.__width * 2) + (self.__height * 2))
 
-        def __str__(self) -> str:
-           """present a diagram of the rec defined for an object"""
-           if self.__width == 0 or self.__height == 0:
-               return ("")
-           rectangle = ""
-           for column in range(self._-height):
-               for row in range(self.__width):
-                   rectangle += "#"
-                if column < self.__height - 1:
-                    rectangle += "\n"
-            return (rectangle)
+    def __str__(self) -> str:
+        """present a diagram of the rec defined for an object"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        rectangle = ""
+        for column in range(self.__height):
+            for row in range(self.__width):
+                rectangle += "#"
+            if column < self.__height - 1:
+                rectangle += "\n"
+        return (rectangle)
 
-        def __repr__(self):
-            return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
