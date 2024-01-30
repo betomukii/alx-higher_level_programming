3856a914b9aca64c3ad8942ad1ef015b65a6d60c
betomukii <mukiibeatrice4@gmail.com>
2024-01-30 15:19:04 +0300

    changes

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -58,23 +58,22 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self) -> str:
-            """presents a diagram of the rectangle defined for an object"""
-            if self.__width == 0 or self.__height == 0:
-                return ("")
-            rectangle = ""
-            for column in range(self.__height):
-                for row in range(self.__width):
-                    rectangle += "#"
-                if column < self.__height - 1:
-                    rectangle += "\n"
-            return (rectangle)
+        """presents a diagram of the rectangle defined for an object"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        rectangle = ""
+        for column in range(self.__height):
+            for row in range(self.__width):
+                rectangle += "#"
+            if column < self.__height - 1:
+                rectangle += "\n"
+        return (rectangle)
 
-        def __repr__(self):
-            """returns a string representation of the rectangle"""
-            return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+    def __repr__(self):
+        """returns a string representation of the rectangle"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
-        def __del__(self):
-            """prints a message for every object that is deleted"""
-            print("Bye rectangle...")
-            Rectangle.number_of_instances -= 1
-
+    def __del__(self):
+        """prints a message for every object that is deleted"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
