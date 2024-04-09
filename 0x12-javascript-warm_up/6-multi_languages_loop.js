54f599882e869bf53f04bea88614cb20133f0f5c
betomukii <mukiibeatrice4@gmail.com>
2024-04-09 13:57:35 +0300

    loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const strArray= ['C is fun', 'Python is cool', 'JavaScript ia amazing'];
+const strArray = ['C is fun', 'Python is cool', 'JavaScript ia amazing'];
 for (const i in strArray){
   console.log(strArray[i]);
 }
